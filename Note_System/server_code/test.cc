973486aec1b515d5645bcf6bb3cd341c85b73a81
KexinAnswer <925433202@qq.com>
2019-08-26 22:56:33 +0800

    新增登录接口

---
+++ Note_System/server_code/test.cc
@@ -0,0 +1,13 @@
+#include "httplib.h"
+int main()
+{
+  using namespace httplib;
+  Server server;
+  server.Get("/", [](const Request &req, Response &resp) {
+    (void)req;
+    resp.set_content("<html>hello</html>", "text/html");
+  });
+  server.set_base_dir("./wwwroot");
+  server.listen("0.0.0.0", 9092);
+  return 0;
+}
